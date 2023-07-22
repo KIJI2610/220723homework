793c1132c82f809b1a46b82a0e21e2d571b6f9c2
Alexey <kalnitskiy2018@mail.ru>
2023-07-22 15:21:19 +0500

    ex1

---
+++ ex1/Program.cs
@@ -0,0 +1,10 @@
+﻿System.Console.Write("Введите число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int c = a;
+
+for(int i = 1; i < b; i++){
+    a = a * c;
+}
+System.Console.WriteLine(a);
