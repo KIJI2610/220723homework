875eb42b802f8db0275e1e813e04de677cb04fd6
Alexey <kalnitskiy2018@mail.ru>
2023-07-22 15:44:54 +0500

    ex2

---
+++ ex2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int sum = SumDigits(num);
+Console.WriteLine("Сумма цифр в числе: " + sum);
+static int SumDigits(int num)
+{
+    int sum = 0;
+    while (num != 0)
+    {
+            sum += num % 10;
+            num /= 10; 
+    }
+    return sum;
+}
